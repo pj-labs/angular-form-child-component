256bcffa24ac12a75a816ceb91838ac9bbc830d3
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-10-29 12:33:44 -0400

    Fix input labels attribute and update README
    Updated `for` attribute on labels in the form component to correctly match associated input fields, improving accessibility. The changes were made to the `app.component.html` file. The `for` attribute was modified from "name" to "firstName" and "lastName" respectively for consistency and to prevent potential confusion.
    
    Additionally, updated `README.md` to better describe the purpose and functionality of the project. The project title was changed and an illustrative code block was added to demonstrate how a form is structured in the component. Some irrelevant sections were also removed in order to enhance readability. The changes provide more clarity and context to users or different stakeholders who may refer to the README for understanding the project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { ChildComponent } from './child.component';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, ReactiveFormsModule,ChildComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'angular-form-child-component';
